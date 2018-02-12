ea4b870904a612261075a78a32af05735221a91f
esmexx <xiaoxuanzhang9@gmail.com>
2018-02-12 16:13:41 -0500

    Add usage of the Iterate function

--- Iterate.m
+++ Iterate.m
@@ -9,3 +9,7 @@ function handle = Iterate(n , N)
         n = n + 1;
     end
 end
+
+% usage:
+%    iterator = Iterate(1, 10);   % display from 1 to 10 with increment of 1
+%    while iterator(); end;
